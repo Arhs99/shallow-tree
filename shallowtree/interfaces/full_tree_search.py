da2462ef7f979e0d0417286d000aab2a8eb79f54
kostas <kostasp97@gmail.com>
2025-04-14 00:39:31 +0300

    searchcli

--- shallowtree/interfaces/full_tree_search.py
+++ shallowtree/interfaces/full_tree_search.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import time
 import json
 from collections import defaultdict
 from typing import TYPE_CHECKING
@@ -18,12 +17,9 @@ if TYPE_CHECKING:
     from shallowtree.chem import RetroReaction
     from shallowtree.utils.type_utils import (
         Callable,
-        Dict,
         List,
         Optional,
         StrDict,
-        Tuple,
-        Union,
     )
 
 
@@ -46,15 +42,12 @@ class Expander:
         self.expansion_policy = self.config.expansion_policy
         self.filter_policy = self.config.filter_policy
         self.stock = self.config.stock
-
         self.max_depth = 2
 
     def context_search(self, smiles: List[str], scaffold_str: str, max_depth=2) -> pd.DataFrame:
         self.max_depth = max_depth
         rows = []
         for smi in smiles:
-            start = time.time()
-            print(smi)
             solution = defaultdict(list)
             mol = TreeMolecule(parent=None, smiles=smi)
             scaffold = Chem.MolFromSmarts(scaffold_str)
@@ -85,13 +78,9 @@ class Expander:
                         if score > 0.9:
                             self.solved[mol.inchi_key] = (reactants[0], score, action.metadata['classification'])
                         break
-            print(
-                f'Score: {score}  in {time.time() - start} sec  {self._counter} evaluations  {self._cache_counter} cache hits')
             if score > 0.9:
                 self.reconstruct_tree(mol, 0, solution)
-                json_data = json.dumps(dict(solution), indent=2)
-                print(json_data)
-            print('*' * 20)
+                # json_data = json.dumps(dict(solution), indent=2)
             rows.append({'SMILES': smi, 'score': score, 'route': dict(solution), 'BBs': self.BBs})
         df = pd.DataFrame(rows)
         return df
@@ -99,7 +88,6 @@ class Expander:
     def search_tree(
             self,
             smiles: List[str],
-            filter_func: Optional[Callable[[RetroReaction], bool]] = None,
             max_depth=2
     ) -> pd.DataFrame:
         """
@@ -107,8 +95,6 @@ class Expander:
         self.max_depth = max_depth
         rows = []
         for smi in smiles:
-            start = time.time()
-            print(smi)
             solution = defaultdict(list)
             mol = TreeMolecule(parent=None, smiles=smi)
             self.solved = dict()
@@ -117,14 +103,9 @@ class Expander:
             self._counter = 0
             self._cache_counter = 0
             score = self.req_search_tree(mol, depth=0)
-            print(
-                f'Score: {score}  in {time.time() - start} sec  {self._counter} evaluations  {self._cache_counter} cache hits')
             if score > 0.9:
                 self.reconstruct_tree(mol, 0, solution)
-                json_data = json.dumps(dict(solution), indent=2)
-                print(json_data)
-
-            print('*' * 20)
+                # json_data = json.dumps(dict(solution), indent=2)
             rows.append({'SMILES': smi, 'score': score, 'route': dict(solution), 'BBs': self.BBs})
         df = pd.DataFrame(rows)
         return df
@@ -134,7 +115,6 @@ class Expander:
             return 0.0
         self._counter += 1
         if mol in self.stock:
-            # print(f'depth: {depth}  molecule in stock: {mol.smiles}')
             return 1.0
         if mol.inchi_key in self.cache.keys():
             self._cache_counter += 1
@@ -159,8 +139,6 @@ class Expander:
             if score > 0.9:
                 self.solved[mol.inchi_key] = (reactants[0], score, action.metadata['classification'])
                 self.cache[mol.inchi_key] = (depth, score)
-                # A = [x.smiles for x in reactants[0]]
-                # print(f'depth: {depth}  {mol.smiles}>>{A}')
                 return score
         self.cache[mol.inchi_key] = (depth, score)
         return score
@@ -178,63 +156,3 @@ class Expander:
                 for x in rxn:
                     self.reconstruct_tree(x, depth + 1, tree)
                 return
-
-
-if __name__ == '__main__':
-    smiles = [
-        "CN(C)CC(O)COc1ccc(Nc2nccc(N(Cc3ccccc3)c3cc(Cl)ccc3Cl)n2)cc1",
-        "O=C(Nc1n[nH]c2cc(-c3ccccc3)ccc12)C1CC1",
-        "O=C(Nc1n[nH]c2nc(-c3ccc(O)c(Br)c3)ccc12)C1CC1",
-        "O=C(Nc1ccc(N2CCNCC2)cc1)c1n[nH]cc1Nc1cc(Oc2ccccc2)ncn1",
-        "CN(C)CC(O)COc1ccc(Nc2cc(Nc3ccccc3F)ncn2)cc1",
-        "CC(C)n1cnc2c(N)nc(NCCCO)nc21",
-        "Cc1ccc2[nH]cc(C(=O)C(=O)N3CCc4ccccc43)c2c1",
-        "CCSc1nn2c(Cc3ccccc3)nnc2s1",
-        "CCc1cc2c(N/N=C\c3cccs3)nc(-c3ccccc3)nc2s1",
-        "NCC(=O)NS(=O)(=O)c1ccc(Nc2nc(N)n(C(=O)c3c(F)cccc3F)n2)cc1",
-        "CC1(C)OCC(=O)Nc2cc(Nc3nc(NCC(F)(F)F)c4occc4n3)ccc21",
-        "NS(=O)(=O)c1ccc(N/N=C2\C(=O)Nc3cc(Br)ccc32)cc1",
-        "O=C1Nc2ccc(F)c3c(OCCCO)cc(-c4ccc[nH]4)c1c23",
-        "COc1cc(C)c(Sc2cnc(NC(=O)c3ccc(CNC(C)C(C)(C)C)cc3)s2)cc1C(=O)N1CCN(C(C)=O)CC1",
-        "Cc1ccc(Nc2cc(Nc3ccc(OCC(O)CN(C)C)cc3)ncn2)cc1",
-        "CC(C)(C)c1ccc(Nc2nccc(-c3cnn4ncccc34)n2)cc1",
-        "COc1cc(O)c2c(c1)C(=O)c1cc(C)c(O)c(O)c1C2=O",
-        "CC(C)n1cnc2c(NC3CCCC3)nc(NCCCO)nc21",
-        "O=C(Nc1n[nH]c2cc(-c3ccc(F)cc3)ccc12)C1CC1",
-        "O=C(Nc1n[nH]c2nc(-c3ccco3)c(Br)cc12)C1CCN(Cc2ccccc2)C1",
-        "CC(=O)Nc1c[nH]nc1-c1nc2ccccc2[nH]1",
-        "O=C(Nc1c[nH]nc1-c1nc2ccc(CN3CCOCC3)cc2[nH]1)c1ccc(F)cc1",
-        "O=C(Nc1c[nH]nc1-c1nc2cc(CN3CCOCC3)ccc2[nH]1)Nc1c(F)cccc1F",
-        "CC1NCCCC1C(=O)Nc1ncc(SCc2ncc(C(C)(C)C)o2)s1",
-        "O=C(Nc1n[nH]c2cc(-c3cccs3)ccc12)C1CC1",
-        "CC(C)(C)c1cnc(CSc2cnc(NCC3CCNCC3)s2)o1",
-        "Cc1ccc(C(=O)NC2CC2)cc1-c1ccc2c(C3CCNCC3)noc2c1",
-        "CN(C)c1cc2c(Nc3ccc4c(cnn4Cc4ccccc4)c3)ncnc2cn1",
-        "Nc1ccccc1NC(=O)c1ccc(-c2ccc3ncnc(Nc4ccc(OCc5cccc(F)c5)c(Cl)c4)c3c2)o1",
-        "N#Cc1c(NC(=O)c2cccc3ccccc23)sc2c1CCCC2",
-        "NS(=O)(=O)c1ccc(N/N=C2\C(=O)Nc3ccc4ncsc4c32)cc1",
-        "NS(=O)(=O)c1ccc(N/N=C2\C(=O)Nc3cc(Oc4ccccc4)ccc32)cc1",
-        "CNC(=O)c1nn(C)c2c1C(C)(C)Cc1cnc(Nc3ccc(CN4CCN(C)CC4)cc3)nc1-2",
-        "CC(C)(C)c1cc2c(N/N=C\c3cccc(CN)n3)ncnc2s1",
-        "COc1cccc2c1c(Cl)c1c3c(cc(O)c(O)c32)C(=O)N1",
-    ]
-    filename = '/home/kostas/data/aizynth/config.yml'
-    start = time.time()
-    expander = Expander(configfile=filename)
-    expander.expansion_policy.select_first()
-    print(f'Load exp policy: {time.time() - start} sec')
-    expander.filter_policy.select_first()
-    print(f'Load filter policy: {time.time() - start} sec')
-    expander.stock.select_first()
-    print(f'Load stock: {time.time() - start} sec')
-
-    df = expander.context_search(
-        ['Clc1ccccc1COC5CC(Nc3n[nH]c4cc(c2ccccc2)ccc34)C5'], scaffold_str='[*]c1n[nH]c2cc(-c3ccccc3)ccc12'
-    )
-
-    # df = expander.search_tree(
-    #     smiles[:5]
-    #     # ['C(Nc1n[nH]c2cc(-c3ccccc3)ccc12)C1CC1']
-    #     # 'Cc1cccc(c1N(CC(=O)Nc2ccc(cc2)c3ncon3)C(=O)C4CCS(=O)(=O)CC4)C'
-    # )
-    print(df.to_csv())
--- shallowtree/interfaces/searchcli.py
+++ shallowtree/interfaces/searchcli.py
@@ -1,6 +1,3 @@
-""" Module containing classes and routines for the CLI
-"""
-
 from __future__ import annotations
 
 import sys
@@ -11,38 +8,51 @@ import argparse
 def main():
     # Set up command-line argument parser
     parser = argparse.ArgumentParser(
-        description="Retrosynthetic analysis and scoring"
+        description="Retrosynthetic analysis and scoring\n"
+                    "Loads a list of SMILES from stdin, outputs a csv file with scores, predicted routes and required available starting materials",
+        epilog="Example usage:\n"
+               "echo 'Clc1ccccc1COC5CC(Nc3n[nH]c4cc(c2ccccc2)ccc34)C5' | searchcli --config config.yml --scaffold '[*]c1n[nH]c2cc(-c3ccccc3)ccc12' --depth 2 --routes\n"
+               "searchcli --config config.yml --depth 2 --routes <smiles.txt >routes.csv",
+        formatter_class=argparse.RawDescriptionHelpFormatter
     )
 
     parser.add_argument(
-        "--config",
+        '--config',
         type=str,
         required=True,
-        help="path to config.yml"
+        help='path to configuration yml file'
     )
 
     parser.add_argument(
-        "--depth",
+        '--depth',
         type=int,
         required=True,
-        help="Search depth for molecular analysis"
+        help='depth of retrosynthetic tree'
     )
 
     # Optional arguments
     parser.add_argument(
-        "--scaffold",
+        '--scaffold',
         type=str,
         default=None,
-        help="Scaffold SMILES string to use as reference (optional)"
+        help='Scaffold SMILES for context search, e.g. \'[*]c1n[nH]c2cc(-c3ccccc3)ccc12\''
     )
 
     parser.add_argument(
-        "--routes",
-        action="store_true",
-        help="Enable generation of synthetic routes"
+        '--routes',
+        action='store_true',
+        help='Enable generation of synthetic routes'
     )
 
+    if '-h' in sys.argv or '--help' in sys.argv:
+        parser.print_help()
+        sys.exit(0)
+
     # Parse arguments
+    if sys.stdin.isatty():
+        print('No SMILES input was detected!', file=sys.stderr)
+        print('Use -h or --help for help', file=sys.stderr)
+        sys.exit(1)
     smiles = [x.strip() for x in sys.stdin]
     args = parser.parse_args()
     expander = Expander(configfile=args.config)
@@ -58,6 +68,9 @@ def main():
     if not args.routes:
         df = df.drop(columns=['route'])
 
-    print(df.to_csv())
-if __name__ == "__main__":
+    df.to_csv(sys.stdout, index=False) # NOT for very large files that can overflow the stdout buffer, can use chunks etc
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
     main()
