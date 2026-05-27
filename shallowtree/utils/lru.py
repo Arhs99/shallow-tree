4b2043fb44599bc70e47d1e68a37215bcca7d986
patronov <patronov@gmail.com>
2026-05-27 16:42:34 +0200

    Resolving circular dependecies.

--- shallowtree/utils/lru.py
+++ shallowtree/utils/lru.py
@@ -11,7 +11,7 @@ from __future__ import annotations
 from collections import OrderedDict
 from typing import Optional
 
-from shallowtree.chem.mol import TreeMolecule #TODO: fix the circular dependency
+from shallowtree.chem.mol import TreeMolecule
 
 
 class LRUCache:
@@ -23,15 +23,15 @@ class LRUCache:
         if maxsize < 1:
             raise ValueError("maxsize must be >= 1")
         self._maxsize = maxsize
-        self._data: OrderedDict[str, "TreeMolecule"] = OrderedDict()
+        self._data: OrderedDict[str, TreeMolecule] = OrderedDict()
 
-    def get(self, key: str, default: Optional["TreeMolecule"] = None) -> Optional["TreeMolecule"]:
+    def get(self, key: str, default: Optional[TreeMolecule] = None) -> Optional[TreeMolecule]:
         if key in self._data:
             self._data.move_to_end(key)
             return self._data[key]
         return default
 
-    def __setitem__(self, key: str, value: "TreeMolecule") -> None:
+    def __setitem__(self, key: str, value: TreeMolecule) -> None:
         if key in self._data:
             self._data.move_to_end(key)
             self._data[key] = value
